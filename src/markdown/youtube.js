47346bdabb8a1d2fce21fcaf12052e1b9cdc52f8
Roman Ožana <roman@ozana.cz>
2025-12-18 09:41:23 +0100

    Add youtu.be support

--- src/markdown/youtube.js
+++ src/markdown/youtube.js
@@ -1,4 +1,4 @@
-import {visit} from 'unist-util-visit';
+import { visit } from 'unist-util-visit';
 
 export default function youtube() {
 
@@ -8,13 +8,17 @@ export default function youtube() {
 			// Auto embed YouTube videos
 			if (URL.canParse(node.url)) {
 				const video = new URL(node.url);
-				if (video.hostname.endsWith('youtube.com')) {
-					const id = video.searchParams.get('v');
 
-					if (id) {
-						node.type = 'html';
-						node.value = `<iframe src="https://www.youtube.com/embed/${id}?rel=0&controls=1" allowfullscreen  style="aspect-ratio: 16 / 9; width: 100%; border: 0"></iframe></>`;
-					}
+				let id = null;
+				if (video.hostname.endsWith('youtube.com')) {
+					id = video.searchParams.get('v');
+				} else if (video.hostname.endsWith('youtu.be')) {
+					id = video.pathname.slice(1);
+				}
+
+				if (id) {
+					node.type = 'html';
+					node.value = `<iframe src="https://www.youtube.com/embed/${id}?rel=0&controls=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="aspect-ratio: 16 / 9; width: 100%; border: 0"></iframe>`;
 				}
 			}
 		});
